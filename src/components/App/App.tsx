3f340b643f46920d32d5d66485b469003c5821e1
Vera Chashchina <chaschinavera@gmail.com>
2024-04-27 23:24:26 +0300

    add ts in project

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from '../Button/Button';
+import React from 'react';
+
+function App() {
+  return (
+    <div className='app'>
+      <Button 
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function Button() {
+    return (
+        <div className="button">
+            <p>{}</p>
+        </div>
+    )
+};
+
+export default Button;
